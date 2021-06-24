1b38a689ac1ba34357dea080455e1f926df47d00
Suale <sualeabdul@gmail.com>
2021-06-24 12:39:08 +0000

    new components created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import HomePage from './homePage.component';
+import HomePage from './components/pages/homepage/homepage.component';
 
 function App() {
   return (
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './menu-item.style.scss'
+
+const MenuItem = ({title,imageUrl,size}) => {
+    return (
+        <div  className={`menu-item ${size}`}>
+        <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}}></div>
+                <div className="content">
+                    <h1 className="title">{title}</h1>
+                    <span className="subtitle">shop Now</span>
+                </div>
+        </div>
+    );
+}
+
+export default MenuItem;
